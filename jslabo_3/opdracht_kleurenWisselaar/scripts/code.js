85352ff74682632e0389f95c776ccb5ab593f253
RobinRlndt <robin.roelandt@student.vives.be>
2025-02-19 21:41:24 +0100

    JSLABO_3

---
+++ jslabo_3/opdracht_kleurenWisselaar/scripts/code.js
@@ -0,0 +1,8 @@
+const setup = () => {
+	document.querySelectorAll("input[type='button']").forEach(button =>
+		button.addEventListener("click", () => button.classList.toggle("active"))
+	);
+};
+
+window.addEventListener("load", setup);
+
